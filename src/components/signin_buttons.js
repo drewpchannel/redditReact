ab54fdeb797dd77e8904f318c62af91399e1c4b3
Drew Poulin <drewpcchannel@gmail.com>
2016-12-01 13:40:35 -0500

    found google sign in email

--- src/components/signin_buttons.js
+++ src/components/signin_buttons.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
 //make this an array and load in each website login?
-const SignIn = (props) => {
-  return (
-    <div className="g-signin2" data-onsuccess="onSignIn"></div>
-  )
+class SignIn extends Component {
+  constructor (props) {
+    super(props);
+  }
+  componentDidMount () {
+    this.onSignIn()
+  }
+  onSignIn () {
+    //find a way to replace the setTimeout.  mayeb redo google login? https://developers.google.com/identity/sign-in/web/reference
+    if (typeof(gapi.auth2) === "object") {
+      window.setTimeout(()=>
+        {
+          if (gapi.auth2.getAuthInstance().currentUser.Ab.w3 !== undefined) {
+            console.log(gapi.auth2.getAuthInstance().currentUser.Ab.w3.U3)
+          }  
+        }, 500)
+    }
+  }
+  render () {
+    return (
+      <div className="g-signin2" data-onsuccess={this.onSignIn()}></div>
+    )
+  }
 }
 
 export default SignIn;
