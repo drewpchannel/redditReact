040c48c5c9f6fd6c16288db9fd7feb9ba632bcb1
drewpchannel <drewpcchannel@gmail.com>
2017-01-12 16:42:22 -0500

    added in support for unformatted links

--- src/functions/parseLinks.js
+++ src/functions/parseLinks.js
@@ -1,18 +1,19 @@
 import React from 'react';
 function parseLinks (comment, id) {
-	if (comment.indexOf('(htt') === -1) {
+	if (comment.indexOf('htt') === -1) {
     return <p className="authorCommentBody">{comment}</p>
   }
 	let currentComment = comment.split(' ');
   for (let x = 0; x < currentComment.length; x++) {
     let linkURL;
     let linkText = 'Link';
-    if (currentComment[x].indexOf('(htt') !== -1) {
+      console.log(currentComment[x])
+    if (currentComment[x].indexOf('htt') !== -1) {
       if (currentComment[x].indexOf(']') !== -1) {
         let g = currentComment[x].substring(0, currentComment[x].indexOf(']')) + "] ";
         currentComment[x - 1] = currentComment[x - 1] + " " + g;
       }
-      linkURL = currentComment[x].substring(currentComment[x].indexOf('(htt') + 1, currentComment[x].indexOf(')'))
+      linkURL = currentComment[x].substring(currentComment[x].indexOf('htt') + 1, currentComment[x].indexOf(')'))
       currentComment[x] = <a href={linkURL} key={id + x}>Link </a>;
     } else {
       currentComment[x] = `${currentComment[x]} `
