8e12693dd959645902df830b6e51d54e9cca68dd
D <drewpcchannel@gmail.com>
2018-05-14 22:09:07 -0600

    gifs now play only when hovered over, fixed videos

--- expressHost.js
+++ expressHost.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 8081;
 
-app.listen(8080, () => {
-	console.log('listening 8080...');
+app.get('/', (req, res) => {
+	console.log('this runs')
+	res.sendFile(path.join(__dirname, 'public'));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port, () => {
+	console.log(`Trying to load port file or port: ${port}`);
+});
--- src/components/create_items.js
+++ src/components/create_items.js
@@ -4,14 +4,29 @@ import CommentButton from './comments_button';
 const redditItemsList = (redditItems, redditListRef) => {
   let redditItemsComplete = redditItems.map((elem) => {
     let imageDefault = "http://images.clipartpanda.com/white-cloud-clipart-no-background-13270607091459405201simplecloud-bw.svg";
+    let externalLink;
     let imageHeight = 300;
     let imageWidth = 300;
+    let elemID = elem.data.id + "img";
+    let hoverPlayGif = () => {};
+    let stopGif = () => {};
     if (elem.data.preview) {
-      const redditPost = elem.data.preview.images[0]
+      const redditPost = elem.data.preview.images[0];
       if (redditPost.variants.gif) { 
         imageDefault = elem.data.thumbnail;
+        externalLink = redditPost.variants.gif.source.url;
+        hoverPlayGif = () => {
+          document.getElementById(elemID).src = redditPost.variants.gif.source.url;
+        }
+        stopGif = () => {
+          document.getElementById(elemID).src = elem.data.thumbnail;
+        }
+      } else if (elem.data.post_hint === "hosted:video") {
+        imageDefault = redditPost.source.url;
+        externalLink = elem.data.url;
       } else {
         imageDefault = redditPost.source.url;
+        externalLink = redditPost.source.url;
       }
     }
     let divSize = {
@@ -20,8 +35,8 @@ const redditItemsList = (redditItems, redditListRef) => {
     let date = new Date(elem.data.created).toString();
     return (
       <div key={elem.data.id + "div"} id={elem.data.id + "div"} style={divSize} className="redditItemBox" ref={elem.data.id} >
-            <a href={imageDefault} target="_blank" className="itemImageLink">
-              <img key={elem.data.id + "img"} src={imageDefault} className="redditItemImage" />
+            <a href={externalLink} target="_blank" className="itemImageLink">
+              <img key={elemID} id={elemID} src={imageDefault} className="redditItemImage" onMouseOver={hoverPlayGif} onMouseLeave = {stopGif} />
             </a>
           <div className="RedditPostDivs">
             <a key={elem.data.id} href={`https://www.reddit.com${elem.data.permalink}`} target="_blank" className="redditLinkText">{elem.data.title}</a>
